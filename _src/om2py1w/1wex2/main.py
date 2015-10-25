6f3bddbf602500b64167d56c31d4fd935b8d49e7
Knight <861943967@qq.com>
2015-10-25 19:55:04 +0800

    new

--- _src/om2py1w/1wex2/main.py
+++ _src/om2py1w/1wex2/main.py
@@ -1,25 +1,29 @@
-#!/user/bin/env python
-#_*_coding:utf-8_*_
+﻿#_*_coding:utf-8_*_
 
 import time
-print '-'*22
-f=open('c:\shaowan\diary.txt','r')
-for line in f:
-    print line
+import os
+
+filename='diary.txt'
+if not os.path.exists(filename):
+    f=open(filename,'w')
+else:
+    f=open(filename,'r')
+    for line in f:
+        print line
 f.close()
 option=raw_input('continue to write? yes/no')
 while option=='yes':
-    fw=open('c:\shaowan\diary.txt','a')
+    f=open(filename,'a')
     fl=raw_input('you can write here')
     save=raw_input('save or not or save and quit')
     localtime=time.asctime(time.localtime(time.time()))
     if save=='save':
-        fw.write(fl+'\n')
-        fw.close()
+        f.write(fl+'\n')
+        f.close()
     elif save=='not':
-        fw.close()
+        f.close()
     else:
-        fw.write(fl+'\n'+localtime)
-        fw.close()
+        f.write(fl+'\n'+localtime)
+        f.close()
         break
-else:fw.close()
+else:f.close()
